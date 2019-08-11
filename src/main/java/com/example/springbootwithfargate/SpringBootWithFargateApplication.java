263a704de0202d409500dae13c9a392fb0d4c766
anhtv08 <anhtv010888@gmail.com>
2019-08-11 15:46:28 +0800

    inital commit

---
+++ src/main/java/com/example/springbootwithfargate/SpringBootWithFargateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithfargate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithFargateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithFargateApplication.class, args);
+    }
+
+}
